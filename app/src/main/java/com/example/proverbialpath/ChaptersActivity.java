8c1a538a127e07c31f48b0f2673302c7a480a1ff
Keziah <keziah.mariz12@gmail.com>
2024-01-03 21:27:57 +0800

    Remade the Search Bar on Chapters Window, started the Results Page but unfinished

--- app/src/main/java/com/example/proverbialpath/ChaptersActivity.java
+++ app/src/main/java/com/example/proverbialpath/ChaptersActivity.java
@@ -10,4 +10,5 @@ public class ChaptersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chapters);
     }
+
 }
---
+++ app/src/main/java/com/example/proverbialpath/SearchItem.java
@@ -0,0 +1,24 @@
+package com.example.proverbialpath;
+
+public class SearchItem {
+    private int mImageResource;
+    private String mText1;
+    private String mText2;
+
+    public SearchItem(int imageResource, String text1, String text2) {
+        mImageResource = imageResource;
+        mText1 = text1;
+        mText2 = text2;
+    }
+    public int getmImageResource() {
+        return mImageResource;
+    }
+
+    public String getText1() {
+        return mText1;
+    }
+
+    public String getText2() {
+        return mText2;
+    }
+}
